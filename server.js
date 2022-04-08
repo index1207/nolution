8dcb8c023e3bd36ca81cd8194add1f3bf03f6570
index1207 <han93851207@gmail.com>
2022-04-08 10:29:15 +0900

    이것저것 추가

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const http = require('http')
-const path = require('path')
+const express = require('express') // 웹서버 구축
+const router = express.Router() //
+const http = require('http') // http프로토콜
+const path = require('path') // 디렉토리
 
-let app = express();
+const mysql = require('mysql') // 유저 관리
+
+const app = express();
 
 app.get('/*', function(req, res) {
     res.sendFile(path.resolve(__dirname, './', 'index.html'));
